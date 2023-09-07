0b56f336d3d9a3600db6566e29499b9831aa6191
brunoamorim145 <136940692+brunoamorim145@users.noreply.github.com>
2023-09-07 11:43:33 -0300

    alterado com alguns testes

--- desafio.kt
+++ desafio.kt
@@ -2,7 +2,7 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario (var nome: String, val id: Int)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
@@ -11,11 +11,32 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val aluno1 = Usuario("Josefina", 1)
+    val aluno2 = Usuario("Durquesa", 2)
+    val aluno3 = Usuario("Girondino", 3)
+    val aluno4 = Usuario("Gusmao", 4)
+
+    val c1 = ConteudoEducacional("Kotlin", 60)
+    val c2 = ConteudoEducacional("C++", 50)
+
+    val f1 = Formacao("Kotlin", mutableListOf(c1, c2))
+    val f2 = Formacao("C++", mutableListOf(c1, c2))
+    val f3 = Formacao("C++", mutableListOf(c1, c2))
+    val f4 = Formacao("Kotlin", mutableListOf(c1, c2))
+    
+    f1.matricular(aluno1)
+    f2.matricular(aluno2)
+    f3.matricular(aluno3)
+    f4.matricular(aluno4)
+
+    println("Alunos matriculados")
+    f1.inscritos.forEach { aluno ->
+        println("Nome: ${aluno.nome}, ID: ${aluno.id}")
+    }
+
 }
